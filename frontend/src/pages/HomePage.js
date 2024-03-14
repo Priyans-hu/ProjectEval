061ce075025e6058c567a9de63e3086391270789
priyans-hu <gargpriyanshu2004@gmail.com>
2024-03-15 03:36:48 +0530

    Implemented download as excel functionality

--- frontend/src/pages/HomePage.js
+++ frontend/src/pages/HomePage.js
@@ -4,6 +4,8 @@ import mentorApi from '../api/MentorApi';
 import StudentCard from '../components/StudentCard';
 import { useNavigate } from 'react-router-dom';
 
+import DownloadXLSX from '../utils/DownloadXLSX';
+
 const HomePage = () => {
     const mentorUserId = localStorage.getItem('mentorUserId');
     const [students, setStudents] = useState([]);
@@ -52,7 +54,7 @@ const HomePage = () => {
                 <div>
                     <div className='flex items-center justify-between my-4'>
                         <h1 className="text-xl md:text-3xl font-bold">Selected Students</h1>
-                        <button className='py-2 px-6 bg-blue-400 rounded-xl font-bold' onClick={handleEditClick}>Edit</button>
+                        <button className='py-2 px-6 bg-blue-400 rounded font-bold' onClick={handleEditClick}>Edit</button>
                     </div>
                     <ul className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4">
                         {selectedStudents.map(student => (
@@ -65,17 +67,20 @@ const HomePage = () => {
                 <div>
                     <div className='flex items-center justify-between'>
                         <h1 className="text-xl md:text-3xl font-bold mt-8 mb-4">All Students</h1>
-                        <div className='text-sm md:text-base flex items-center'>
-                            <p className='mr-2'>Filter:</p>
-                            <select
-                                className='py-2 px-4 bg-gray-200 rounded'
-                                value={filter}
-                                onChange={e => setFilter(e.target.value)}
-                            >
-                                <option value="all">All</option>
-                                <option value="assigned">Assigned</option>
-                                <option value="pending">Pending</option>
-                            </select>
+                        <div className='flex gap-3'>
+                            <div className='text-sm md:text-base flex items-center'>
+                                <p className='mr-2'>Filter:</p>
+                                <select
+                                    className='py-2 px-4 bg-gray-200 rounded'
+                                    value={filter}
+                                    onChange={e => setFilter(e.target.value)}
+                                >
+                                    <option value="all">All</option>
+                                    <option value="assigned">Assigned</option>
+                                    <option value="pending">Pending</option>
+                                </select>
+                            </div>
+                            <button className='px-4 py-2 bg-green-400 rounded'> <DownloadXLSX data={students} /> </button>
                         </div>
                     </div>
                     <ul className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
